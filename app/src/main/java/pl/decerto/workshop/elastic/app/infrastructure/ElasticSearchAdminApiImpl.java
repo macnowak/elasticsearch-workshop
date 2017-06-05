bf73aee61370127f9cd842b4c539c29537edeb4d
Maciej Nowak <maciej.nowak@decerto.pl>
2017-06-06 00:23:57 +0200

    step-1-solution

--- app/src/main/java/pl/decerto/workshop/elastic/app/infrastructure/ElasticSearchAdminApiImpl.java
+++ app/src/main/java/pl/decerto/workshop/elastic/app/infrastructure/ElasticSearchAdminApiImpl.java
@@ -1,9 +1,24 @@
 package pl.decerto.workshop.elastic.app.infrastructure;
 
+import org.elasticsearch.client.Client;
+
 class ElasticSearchAdminApiImpl implements ElasticSearchAdminApi {
+
+	private final Client client;
+
+	ElasticSearchAdminApiImpl(Client client) {
+		this.client = client;
+	}
 
 	@Override
 	public void createIndex(String name, String indexConfigSource) {
+		if (indexExists(name)) {
+			return;
+		}
+		client.admin().indices().prepareCreate(name).setSource(indexConfigSource).get();
+	}
 
+	private boolean indexExists(String name) {
+		return client.admin().indices().prepareExists(name).get().isExists();
 	}
 }
--- app/src/test/groovy/pl/decerto/workshop/elastic/app/infrastructure/ElasticSearchAdminApiImplTest.groovy
+++ app/src/test/groovy/pl/decerto/workshop/elastic/app/infrastructure/ElasticSearchAdminApiImplTest.groovy
@@ -7,15 +7,17 @@ import java.nio.charset.Charset
 
 class ElasticSearchAdminApiImplTest extends EmbeddedElasticTest {
 
-	String indexName = "test_index-v0"
 	String indexConfigFile = "/test_index-v0.json"
 	ElasticSearchAdminApiImpl api
 
 	void setup() {
-		api = new ElasticSearchAdminApiImpl()
+		api = new ElasticSearchAdminApiImpl(client)
 	}
 
 	def "should create index in elastic "() {
+		given:
+		String indexName = "test_index-v0"
+
 		when:
 		api.createIndex(indexName, IOUtils.toString(this.getClass().getResourceAsStream(indexConfigFile), Charset.defaultCharset()))
 
@@ -24,6 +26,9 @@ class ElasticSearchAdminApiImplTest extends EmbeddedElasticTest {
 	}
 
 	def "creating index should be idempotent"() {
+		given:
+		String indexName = "test_index-v1"
+
 		when:
 		api.createIndex(indexName, IOUtils.toString(this.getClass().getResourceAsStream(indexConfigFile), Charset.defaultCharset()))
 
